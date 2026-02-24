e7b77444b82122037a7c1b4f4e2e44b40cee22ca
Prometheuz <gabrielsampaiodejesus01042007@gmail.com>
2026-02-23 21:35:44 -0300

    fix: correção na logica do TokenController e permissão para acessar a rota /login pelo metodo post

--- atlas/src/main/resources/data.sql
+++ atlas/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO role (id, nome) VALUES (1, "ADMIN")
-INSERT INTO role (id, nome) VALUES (2, "PERSONAL")
-INSERT INTO role (id, nome) VALUES (3, "ALUNO")
+INSERT IGNORE INTO role (id, nome) VALUES (1, "ADMIN")
+INSERT IGNORE INTO role (id, nome) VALUES (2, "PERSONAL")
+INSERT IGNORE INTO role (id, nome) VALUES (3, "ALUNO")
