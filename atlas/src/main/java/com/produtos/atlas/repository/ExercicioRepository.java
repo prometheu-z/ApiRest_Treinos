17d288c1586c4946afa2aa84425ca337867b83b7
Prometheuz <gabrielsampaiodejesus01042007@gmail.com>
2026-02-27 18:10:03 -0300

    refactor/feat: correção no ItemTreinoReqDTO e adição da pesquisa de exercicios na rota /treinos

--- atlas/src/main/java/com/produtos/atlas/repository/ExercicioRepository.java
+++ atlas/src/main/java/com/produtos/atlas/repository/ExercicioRepository.java
@@ -1,9 +1,13 @@
 package com.produtos.atlas.repository;
 
 import com.produtos.atlas.model.Exercicio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
+
+    Page<Exercicio> findByGrupoMuscularContainingIgnoreCaseOrNomeContainingIgnoreCase(String grupoMuscular, String nome, Pageable pageable);
 }
---
+++ atlas/src/main/java/com/produtos/atlas/service/ExercicioService.java
@@ -0,0 +1,25 @@
+package com.produtos.atlas.service;
+
+import com.produtos.atlas.model.Exercicio;
+import com.produtos.atlas.repository.ExercicioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ExercicioService {
+
+    @Autowired
+    private ExercicioRepository exercicioRepository;
+
+    public Optional<Exercicio> findById(Long id){
+        return exercicioRepository.findById(id);
+    }
+
+    public Page<Exercicio> buscarExeercicios(String grupoMuscular, String nome, int pagina, int tamanho){
+        return exercicioRepository.findByGrupoMuscularContainingIgnoreCaseOrNomeContainingIgnoreCase(grupoMuscular, nome, PageRequest.of(pagina,tamanho));
+    }
+}
